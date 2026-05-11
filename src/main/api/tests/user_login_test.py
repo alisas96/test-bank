f6cf3eb643095d9b323c679f97ee6d662aeaca79
alisas96 <alisas96@mail.ru>
2026-05-11 12:35:54 +0300

    Update user_login_test.py

--- src/main/api/tests/user_login_test.py
+++ src/main/api/tests/user_login_test.py
@@ -10,13 +10,13 @@ class TestUserLogin:
         login_user_request = LoginUserRequest(username="admin", password="123456")
         response = api_manager.admin_steps.login_user(login_user_request)
 
-        assert login_user_request.username == response.user.username
-        assert response.user.role == "ROLE_ADMIN"
+        assert login_user_request.username == response.user.username, "Имя пользователя не совпадает, ошибка"
+        assert response.user.role == "ROLE_ADMIN", "Роль пользователя не совпадает, ошибка"
 
     def test_login_user(
         self, api_manager: ApiManager, create_user_request: CreateUserRequest
     ):
         response = api_manager.admin_steps.login_user(create_user_request)
 
-        assert create_user_request.username == response.user.username
-        assert response.user.role == "ROLE_USER"
+        assert create_user_request.username == response.user.username, "Имя пользователя не совпадает, ошибка"
+        assert response.user.role == "ROLE_USER", "Роль пользователя не совпадает, ошибка"
